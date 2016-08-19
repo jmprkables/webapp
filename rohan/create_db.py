ba90165730e5676c116b88985f22848db7ef88cc
Rohan Verma <rohanverma2004@gmail.com>
2016-08-20 04:39:29 +0530

    Added db creation script

---
+++ rohan/create_db.py
@@ -0,0 +1,20 @@
+import rethinkdb as r
+
+conn = r.connect("mr-karan.local", 28015)
+try:
+	r.db_drop("hackiiitd").run(conn)
+	print("deleted old db")
+except:
+	print("inital creation")
+
+r.db_create("hackiiitd").run(conn)
+
+r.db("hackiiitd").table_create("fall").run(conn)
+print("."),
+r.db("hackiiitd").table_create("medicine").run(conn)
+print("."),
+r.db("hackiiitd").table_create("door").run(conn)
+print("."),
+r.db("hackiiitd").table_create("photo").run(conn)
+print("."),
+print("done")

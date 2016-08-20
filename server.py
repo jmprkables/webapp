1ab549c3501f166c05a28f98c60706b5592d2012
Karan <karansharma1295@gmail.com>
2016-08-20 07:38:27 +0530

    server

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask import Flask, request
 import rethinkdb as r
+import time
+from datetime import datetime
+
 
 app = Flask(__name__)
 
@@ -9,6 +12,11 @@ def hello_world():
 
 @app.route('/fall', methods=['POST'])
 def fall():
+    timezone = time.strftime("%z")
+    reql_tz = r.make_timezone(timezone[:3] + ":" + timezone[3:])
+    the_date = datetime.now(reql_tz)
+    timestamp = time.mktime(the_date.timetuple())
+    json_date = the_date.isoformat()
     conn = r.connect("mr-karan.local", 28015)
 
     data = request.data
@@ -17,12 +25,50 @@ def fall():
         status = dataDict["status"]
     except:
         return("Invalid data")
-    
-    r.db("hackiiitd").insert({
-        "status": status,
-        "timestamp": adsd
-        })
 
+    r.table("fall").insert({
+        "status": status,
+        'from_object': the_date,
+        'from_epoch': r.epoch_time(timestamp),
+        'from_iso': r.iso8601(json_date)
+    }).run(conn)
+
+@app.route('/medicine', methods=['POST'])
+def medicine():
+    timezone = time.strftime("%z")
+    reql_tz = r.make_timezone(timezone[:3] + ":" + timezone[3:])
+    the_date = datetime.now(reql_tz)
+    timestamp = time.mktime(the_date.timetuple())
+    json_date = the_date.isoformat()
+    conn = r.connect("mr-karan.local", 28015)
+
+    data = request.data
+    dataDict = json.loads(data)
+    try:
+        status = dataDict["status"]
+    except:
+        return("Invalid data")
+
+    r.table("fall").insert({
+        "status": status,
+        'from_object': the_date,
+        'from_epoch': r.epoch_time(timestamp),
+        'from_iso': r.iso8601(json_date)
+    }).run(conn)
+
+@app.route('/door', methods=['POST'])
+def door():
+    data = request.data
+    dataDict = json.loads(data)
+    try:
+        status = dataDict["status"]
+    except:
+        return("Invalid data")
+    # if status is True increase the counter
+    if status:
+        r.table('aggregated').get(id).update(
+            { 'count': (r.row['count'].default(0)+1) }
+        ).run(conn)
 
 
 if __name__ == "__main__":
